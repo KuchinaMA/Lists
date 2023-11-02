aa90a899b53c9b0acbd34615112b0043d7a4c994
KuchinaMA <m.kuchina05@mail.ru>
2023-11-02 16:52:48 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -12,47 +12,13 @@ int main() {
 
     list_constructor(&list);
 
-    //list_dump(&list, __FILE__, __LINE__, __func__);
-
     for (int i = 0; i < 6; i++) {
         list_push(&list, 10*(i + 1), i);
-        //list_dump(&list, __FILE__, __LINE__, __func__);
     }
 
     list_push(&list, 25, 2);
-    //list_dump(&list, __FILE__, __LINE__, __func__);
 
     list_pop(&list, 4);
-    //list_dump(&list, __FILE__, __LINE__, __func__);
-
-    //list_dump_picture(&list);
-
-    /*list_push(&list, 90, 8);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_pop(&list, 8);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_pop(&list, 0);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_pop(&list, 0);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_pop(&list, 4);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_push(&list, 7, 0);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_push(&list, 5, 0);
-    list_dump(&list, __FILE__, __LINE__, __func__);
-
-    list_push(&list, 3, 0);
-    list_dump(&list, __FILE__, __LINE__, __func__);*/
-
-    /*list_pop(&list, 5);
-    list_dump(&list, __FILE__, __LINE__, __func__);*/
 
     list_destructor(&list);
 
